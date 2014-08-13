0d46d4057b599507fd49c0a3a09709a65875e0ee
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-13 15:50:38 -0400

    Part two of exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,9 @@
-grocery_list = ["blueberries"," strawberries","milk"," eggs"]
+grocery_list = ["blueberries","strawberries","milk","eggs"]
 
+def food_list(list)
+list.each do | item |
+ puts "* " + item
+end
+end
+
+food_list(grocery_list)
