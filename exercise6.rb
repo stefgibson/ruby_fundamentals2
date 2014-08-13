e47ae9f8633a63f9e29055a14e56fd3b2fbc5d5d
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-13 16:26:28 -0400

    Here I have calculated the number of items in my list and I have alse checked the list to see if banannas is in it

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,21 @@
 grocery_list = ["blueberries","strawberries","milk","eggs"]
 
 def food_list(list)
-list.each do | item |
- puts "* " + item
+  list.each do | item |
+    puts "* " + item
+  end
 end
+food_list(grocery_list)
+
+puts grocery_list << "rice"
+
+puts grocery_list.length
+
+
+if grocery_list.include? :banannas
+  puts "You need to get banannas today!"
+else
+  puts "You don't need to get banannas!"
 end
 
-food_list(grocery_list)
+
