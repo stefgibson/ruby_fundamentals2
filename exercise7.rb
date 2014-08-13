a2a9c51b3deebba73873fe915c14d14084384d15
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-13 18:08:04 -0400

    completed exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -5,9 +5,31 @@ students = {
 }
 
 def cohort_display(students)
-students.each do |cohort, number|
-  puts "#{cohort} : #{number}"
+  students.each do |cohort, number|
+    puts "#{cohort} : #{number}"
   end
 end
 
 cohort_display(students)
+
+students[:cohort4] = 43
+
+cohort_display(students)
+
+students.each do |key, value|
+  puts "The cohorts are #{key}."
+end
+
+def class_expand(students)
+  students.each do |cohort, number|
+    students[cohort] = number * 1.05
+    end
+end
+
+class_expand(students)
+cohort_display(students)
+
+
+students.delete(:cohort2)
+cohort_display(students)
+
